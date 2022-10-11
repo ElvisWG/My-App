85e8777dcfb1f8cf61eb1cc832dcd3071021d230
Elvis Wanjiru <elvis.gatukui@student.moringaschool.com>
2022-10-11 21:09:55 +0300

    Done

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -18,7 +18,8 @@ const Gallery = props => {
       let url = `https://farm${farm}.staticflickr.com/${server}/${id}_${secret}_m.jpg`;
       return <Image url={url} key={id} alt={title} />;
     });
-    ss
+    
+    
   } else {
     noImages = <NoImages />; // return 'not found' component if no images fetched
   }
